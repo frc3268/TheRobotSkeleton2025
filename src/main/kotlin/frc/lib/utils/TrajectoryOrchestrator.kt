e41fc0f75d0a01d595d7bcc09003eacaff9b677c
Matthew Epshtein <epshteinmatthew@gmail.com>
2023-05-17 14:06:18 -0700

    create & integrate swerve joystick drive command

--- src/main/kotlin/frc/lib/utils/TrajectoryOrchestrator.kt
+++ src/main/kotlin/frc/lib/utils/TrajectoryOrchestrator.kt
@@ -1,5 +1,7 @@
 package frc.lib.utils
 
+import edu.wpi.first.math.geometry.Pose2d
+import edu.wpi.first.math.geometry.Rotation2d
 import edu.wpi.first.math.trajectory.Trajectory
 import edu.wpi.first.wpilibj.shuffleboard.Shuffleboard
 import edu.wpi.first.wpilibj.shuffleboard.ShuffleboardTab
@@ -7,11 +9,20 @@ import edu.wpi.first.wpilibj.shuffleboard.ShuffleboardTab
 class TrajectoryOrchestrator {
     private val tab: ShuffleboardTab = Shuffleboard.getTab("Drivetrain")
     private var trajectory: Trajectory = Trajectory()
-    
+    val startAtRobotPosition : Pose2d = Pose2d(0.0,0.0, Rotation2d.fromDegrees(0.0))
+
     //take in a trajectory and display it on the tab
     
     //update display when trajectory is changed
     
+    fun startSwerveTrajectory(points: Array<Pose2d>) {
+        if(points[0] == startAtRobotPosition){
+            //generate the trajectory at the robot's position
+            return
+        }
+        //generate trajectory normally
+    }
+
     //2 methods to add trajectory: one for swerve and one for differential
     
     //cancel trajectory when updated (?)
--- src/main/kotlin/frc/robot/RobotContainer.kt
+++ src/main/kotlin/frc/robot/RobotContainer.kt
@@ -1,10 +1,14 @@
 package frc.robot
 
+import edu.wpi.first.math.geometry.Pose2d
+import edu.wpi.first.math.geometry.Rotation2d
 import edu.wpi.first.wpilibj2.command.Command
 import edu.wpi.first.wpilibj2.command.button.CommandXboxController
 import edu.wpi.first.wpilibj2.command.button.Trigger
+import frc.lib.basics.SwerveDriveBase
 import frc.robot.commands.Autos
 import frc.robot.commands.ExampleCommand
+import frc.robot.commands.SwerveJoystickDrive
 import frc.robot.subsystems.ExampleSubsystem
 
 /**
@@ -15,13 +19,22 @@ import frc.robot.subsystems.ExampleSubsystem
  */
 class RobotContainer {
     // The robot's subsystems and commands are defined here...
-    private val exampleSubsystem = ExampleSubsystem()
+    private val driveSubsystem:SwerveDriveBase = SwerveDriveBase(Pose2d(0.0,0.0, Rotation2d.fromDegrees(0.0)))
 
     // Replace with CommandPS4Controller or CommandJoystick if needed
     private val driverController = CommandXboxController(Constants.OperatorConstants.kDriverControllerPort)
 
     /** The container for the robot. Contains subsystems, OI devices, and commands.  */
     init {
+        driveSubsystem.setDefaultCommand(
+            SwerveJoystickDrive(
+                driveSubsystem,
+                { -driverController.getLeftY() },
+                { -driverController.getLeftX() },
+                { driverController.getRightX() },
+                { driverController.rightBumper().getAsBoolean() }
+            )
+        )
         // Configure the trigger bindings
         configureBindings()
     }
@@ -35,11 +48,11 @@ class RobotContainer {
      */
     private fun configureBindings() {
         // Schedule ExampleCommand when exampleCondition changes to true
-        Trigger { exampleSubsystem.exampleCondition() }.onTrue(ExampleCommand(exampleSubsystem))
+        //Trigger { exampleSubsystem.exampleCondition() }.onTrue(ExampleCommand(exampleSubsystem))
 
         // Schedule exampleMethodCommand when the Xbox controller's B button is pressed,
         // cancelling on release.
-        driverController.b().whileTrue(exampleSubsystem.exampleMethodCommand())
+        //driverController.b().whileTrue(exampleSubsystem.exampleMethodCommand())
     }
 
     /**
@@ -50,6 +63,6 @@ class RobotContainer {
     val autonomousCommand: Command
         get() {
             // An example command will be run in autonomous
-            return Autos.exampleAuto(exampleSubsystem)
+            return SwerveJoystickDrive(driveSubsystem, {0.1}, {0.0}, {0.0}, {false}).withTimeout(2.0)
         }
 }
