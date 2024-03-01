313cb4c602d94f2d31be6a44773fc2c53571fa60
fernir3268 <116040382+fernir3268@users.noreply.github.com>
2024-02-29 17:08:32 -0800

    Update Autos.kt

--- src/main/kotlin/frc/robot/commands/Autos.kt
+++ src/main/kotlin/frc/robot/commands/Autos.kt
@@ -84,18 +84,15 @@ class Autos private constructor() {
         fun goWithinSpeakerCommand(drive: SwerveDriveBase): Command {
             val color = DriverStation.getAlliance()
             //todo: fix x
-            var to = Pose2d(0.0, 5.547868, 0.0.rotation2dFromDeg())
-            color.ifPresent { color ->
-                if (color == DriverStation.Alliance.Red) {
-                    to = Pose2d(14.579342, 5.547868, 180.0.rotation2dFromDeg())
-                }
-            }
+            val to =
+                if (color.isPresent && color.get() == DriverStation.Alliance.Red)
+                    Pose2d(14.579342, 5.547868, 180.0.rotation2dFromDeg())
+                else
+                    Pose2d(0.0, 5.547868, 0.0.rotation2dFromDeg())
             //todo: make this real
             val pose = drive.getPose()
             val c = 1.0
-            val x = pose.x - to.x
-            val y = pose.y - to.y
-            val theta = atan(y / x).rotation2dFromDeg()
+            val theta = atan((pose.y - to.y) / (pose.x - to.x)).rotation2dFromDeg()
             return TrajectoryOrchestrator.beelineCommand(
                     drive, Pose2d(to.x + cos(theta.radians) * c, to.y + sin(theta.radians) * c, theta + pose.rotation))
         }
