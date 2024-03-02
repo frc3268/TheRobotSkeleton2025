d46752761b34a05769df78ecc76f6b1839cb83dd
fernir3268 <116040382+fernir3268@users.noreply.github.com>
2024-03-02 08:19:48 -0800

    Removed unnecessary imports

--- src/main/kotlin/frc/robot/subsystems/LeftClimberSubsystem.kt
+++ src/main/kotlin/frc/robot/subsystems/LeftClimberSubsystem.kt
@@ -1,6 +1,3 @@
 package frc.robot.subsystems
 
-import com.revrobotics.*
-import edu.wpi.first.wpilibj2.command.*
-
 class LeftClimberSubsystem: ClimberSubsystem(14, 1.0/530)
--- src/main/kotlin/frc/robot/subsystems/RightClimberSubsystem.kt
+++ src/main/kotlin/frc/robot/subsystems/RightClimberSubsystem.kt
@@ -1,6 +1,3 @@
 package frc.robot.subsystems
 
-import com.revrobotics.*
-import edu.wpi.first.wpilibj2.command.*
-
 class RightClimberSubsystem: ClimberSubsystem(15, 1.0/1.0)
