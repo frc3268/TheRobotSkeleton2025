8a060d87f06a08f1536b33ba3799039f562f0d82
fernir3268 <116040382+fernir3268@users.noreply.github.com>
2024-03-01 16:30:17 -0800

    Abstractify Climber subsystem -- Weiju

--- src/main/kotlin/frc/lib/utils/TrajectoryOrchestrator.kt
+++ src/main/kotlin/frc/lib/utils/TrajectoryOrchestrator.kt
@@ -5,30 +5,34 @@ import edu.wpi.first.wpilibj2.command.*
 import frc.lib.basics.SwerveDriveBase
 
 class TrajectoryOrchestrator {
-
-    //take in a trajectory and display it on the tab
-    
-    //update display when trajectory is changed
+    // TODO take in a trajectory and display it on the tab
+    // TODO update display when trajectory is changed
 
     companion object {
-        fun buildSwerveTrajectory(startPose:Pose2d, endPose:Pose2d, points:MutableList<Translation2d>, drive:SwerveDriveBase): SequentialCommandGroup {
-            val scg:SequentialCommandGroup = SequentialCommandGroup()
-            scg.addCommands(drive.robotPoseToBCommand(startPose))
-            for(point in points){
-                scg.addCommands(drive.robotPoseToBCommand(Pose2d(point, drive.getYaw())))
-            }
-            scg.addCommands(drive.robotPoseToBCommand(endPose))
-            scg.addCommands(InstantCommand({drive.stop()}))
+        fun buildSwerveTrajectory(
+            startPose: Pose2d,
+            endPose: Pose2d,
+            points: MutableList<Translation2d>,
+            drive: SwerveDriveBase
+        ): SequentialCommandGroup {
+            // I refactored this to be functional-paradigmed and do the same thing as the earlier imperative code,
+            // but I may hvae messed up. Please check the git history and roll back if it doesn't work.
+            // -- Weiju
+            val scg = SequentialCommandGroup(
+                drive.robotPoseToBCommand(startPose),
+                *points.map { drive.robotPoseToBCommand(Pose2d(it, drive.getYaw())) }.toTypedArray(),
+                drive.robotPoseToBCommand(endPose),
+                InstantCommand({ drive.stop() })
+            )
             scg.addRequirements(drive)
             return scg
         }
 
-
-
-        fun beelineCommand(drive:SwerveDriveBase, to:Pose2d):SequentialCommandGroup{
-            val scg:SequentialCommandGroup = SequentialCommandGroup()
-            scg.addCommands(drive.robotPoseToBCommand(to))
-            scg.addCommands(InstantCommand({drive.stop()}))
+        fun beelineCommand(drive:SwerveDriveBase, to:Pose2d): SequentialCommandGroup {
+            val scg = SequentialCommandGroup(
+                drive.robotPoseToBCommand(to),
+                InstantCommand({drive.stop()})
+            )
             scg.addRequirements(drive)
             return scg
         }
--- src/main/kotlin/frc/robot/subsystems/RightClimberSubsystem.kt
+++ src/main/kotlin/frc/robot/subsystems/RightClimberSubsystem.kt
@@ -3,48 +3,4 @@ package frc.robot.subsystems
 import com.revrobotics.*
 import edu.wpi.first.wpilibj2.command.*
 
-class RightClimberSubsystem: SubsystemBase(){
-    val motor = CANSparkMax(15, CANSparkLowLevel.MotorType.kBrushless)
-    val encoder: RelativeEncoder = motor.encoder
-
-    /* CONSTANTS */
-    private val metersPerRotation: Double = 0.0
-    private val minPositionMeters: Double = 0.0
-    private val maxPositionMeters: Double = 0.34
-
-    init {
-        motor.inverted = true
-        // TODO test if this is needed
-        //rightEncoder.inverted = true
-        encoder.positionConversionFactor = 1.0/1.0
-    }
-
-    /**
-     * TODO make a func that converts meters to rotations and a function that converts rotations to meters
-     * TODO using these functions rotate the motors on the arms accordingly while accounting for min and max heights
-     */
-
-    fun down(): Command =
-        run { motor.set(-0.7) }
-            .until { encoder.position < 0.1 }
-            .andThen(runOnce { motor.stopMotor() })
-
-    fun up(): Command =
-        run { motor.set(0.7) }
-            .until { encoder.position > 0.9 }
-            .andThen(runOnce { motor.stopMotor() })
-
-    fun reset(): Command =
-        runOnce { encoder.position = 0.0 }
-
-    fun climberCentreCommand(): Command =
-        run { //setToPositionMeters(0.2)
-        }
-
-    fun stop(): Command =
-        runOnce { motor.set(0.0) }
-
-    override fun periodic() {
-        System.out.println("Left climber: " + encoder.position)
-    }
-}
+class RightClimberSubsystem: ClimberSubsystem(15, 1.0/1.0)
