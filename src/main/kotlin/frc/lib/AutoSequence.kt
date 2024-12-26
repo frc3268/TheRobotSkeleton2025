1ba797f8796b3105604c7cf2b9b73c64b845532e
epshteinmatthew <64236644+epshteinmatthew@users.noreply.github.com>
2024-12-26 15:55:55 -0800

    fixed auto sequences

--- src/main/kotlin/frc/lib/AutoSequence.kt
+++ src/main/kotlin/frc/lib/AutoSequence.kt
@@ -1,5 +1,6 @@
 package frc.lib
 
+import edu.wpi.first.wpilibj.shuffleboard.Shuffleboard
 import edu.wpi.first.wpilibj2.command.Command
 import edu.wpi.first.wpilibj2.command.ParallelCommandGroup
 import edu.wpi.first.wpilibj2.command.SequentialCommandGroup
@@ -9,14 +10,14 @@ import kotlinx.serialization.json.Json
 @Serializable
 class AutoSequence (val commands:Array<Array<String>>){
     fun toCommandGroup(commandsMap:Map<String, Command>):SequentialCommandGroup{
-        var sq = SequentialCommandGroup()
-        commands.mapIndexed{ _, commandList -> {
-            var pcg = ParallelCommandGroup()
-            commandList.mapIndexed{
-                _, command -> pcg.addCommands(commandsMap[command])
+        val sq = SequentialCommandGroup()
+        for (commandList in commands) {
+            val pcg = ParallelCommandGroup()
+            for (command in commandList) {
+                pcg.addCommands (commandsMap[command])
             }
             sq.addCommands(pcg)
-        } }
+        }
         return sq
     }
 }
