c4685a598cdbf673efd849ddc16d7e92b669349d
epshteinmatthew <64236644+epshteinmatthew@users.noreply.github.com>
2025-01-04 20:43:30 -0800

    comment the pathfind function

--- src/main/kotlin/frc/robot/commands/SwerveAutoDrive.kt
+++ src/main/kotlin/frc/robot/commands/SwerveAutoDrive.kt
@@ -93,37 +93,44 @@ class SwerveAutoDrive(
     private fun pathfind(from: Pose2d, to: Pose2d): MutableList<Pose2d> {
         //this whole thing could be faster...
         val sortedObstacles = obstacles.sortedBy { it.location.translation.getDistance(from.translation) }
+        //get put the line from "from" to "to" into slope-intercept form
         val m: Double = (to.y - from.y) / (to.x - from.x)
         val b: Double = -m * to.x + to.y
         for (obstacle in sortedObstacles) {
+            //get the relevant coefficients from the obstacle's quadratic equation, when substituting mx+b for y
             val ntwo = b - obstacle.location.y
             val obx = obstacle.location.x
+            //quadratic formula constants
             val btwo = -obx * 2 + ntwo * 2 * m
             val a = m.pow(2) + 1
             val c = ntwo.pow(2) + obx.pow(2) - obstacle.radiusMeters.pow(2)
             val det = btwo.pow(2) - 4 * a * c
             if (det >= 0) {
+                //only go forward if there are intersections (that is, if the determinant is non-negative)
                 val intersection_a: Pose2d = if (from.x > obstacle.location.x) Pose2d(
                     ((-btwo + sqrt(det)) / (2 * a)), (m * ((-btwo + sqrt(det)) / (2 * a)) + b), from.rotation
                 ) else Pose2d(
                     ((-btwo - sqrt(det)) / (2 * a)), (m * ((-btwo - sqrt(det)) / (2 * a)) + b), from.rotation
                 )
-                if (intersection_a.x in from.x..to.x || intersection_a.x in to.x..from.x) {
+                if ((intersection_a.x in from.x..to.x || intersection_a.x in to.x..from.x) && (intersection_a.y in from.y..to.y || intersection_a.y in to.y..from.y)) {
+                    //only go forward if the intersection is actually between the points from and to
+                    val intersection_b = if (from.x < obstacle.location.x) Pose2d(
+                        ((-btwo + sqrt(det)) / (2 * a)), (m * ((-btwo + sqrt(det)) / (2 * a)) + b), from.rotation
+                    ) else Pose2d(
+                        ((-btwo - sqrt(det)) / (2 * a)), (m * ((-btwo - sqrt(det)) / (2 * a)) + b), from.rotation
+                    )
 
-                        val intersection_b = if (from.x < obstacle.location.x) Pose2d(
-                            ((-btwo + sqrt(det)) / (2 * a)), (m * ((-btwo + sqrt(det)) / (2 * a)) + b), from.rotation
-                        ) else Pose2d(
-                            ((-btwo - sqrt(det)) / (2 * a)), (m * ((-btwo - sqrt(det)) / (2 * a)) + b), from.rotation
-                        )
-
+                    //midpoint on the line between the two intersection points
                     val linmidpoint = Pose2d(
                         (intersection_b.x + intersection_a.x) / 2,
                         (intersection_b.y + intersection_a.y) / 2,
                         0.0.rotation2dFromDeg()
                     )
 
+                    //how far we have to go to get to the edge of the circle from the linear midpoint
                     val dist = obstacle.radiusMeters - abs(linmidpoint.translation.getDistance(obstacle.location.translation))
 
+                    //rates of change for x and y between the center of the obstacle circle and the linear midpoint, this is used to extend the linear midpoint to the edge of the circle
                     var rates = Pose2d( (dist) * (((linmidpoint.x - obstacle.location.x) / abs((linmidpoint.x - obstacle.location.x))) * linmidpoint.x * (obstacle.radiusMeters/ sqrt(linmidpoint.x + linmidpoint.y)))/obstacle.radiusMeters ,
                         (dist) * (((linmidpoint.y - obstacle.location.y) / abs((linmidpoint.y - obstacle.location.y))) * linmidpoint.y * (obstacle.radiusMeters/ sqrt(linmidpoint.x + linmidpoint.y) )) / obstacle.radiusMeters,
                             from.rotation
@@ -137,8 +144,9 @@ class SwerveAutoDrive(
                         )
 
                     if(waypoint.x !in 0.0..16.4846 || waypoint.y !in 0.0..8.1026) {
-                        rates = Pose2d( (dist) * -(((linmidpoint.x - obstacle.location.x) / abs((linmidpoint.x - obstacle.location.x))) * linmidpoint.x * (obstacle.radiusMeters/ sqrt(linmidpoint.x + linmidpoint.y)))/obstacle.radiusMeters ,
-                            (dist) * -(((linmidpoint.y - obstacle.location.y) / abs((linmidpoint.y - obstacle.location.y))) * linmidpoint.y * (obstacle.radiusMeters/ sqrt(linmidpoint.x + linmidpoint.y) )) / obstacle.radiusMeters,
+                        //if waypoint is outside of the map flip the way the rates go
+                        rates = Pose2d( (abs(obstacle.radiusMeters * 2 - dist)) * -(((linmidpoint.x - obstacle.location.x) / abs((linmidpoint.x - obstacle.location.x))) * linmidpoint.x * (obstacle.radiusMeters/ sqrt(linmidpoint.x + linmidpoint.y)))/obstacle.radiusMeters ,
+                            (abs(obstacle.radiusMeters * 2 - dist)) * -(((linmidpoint.y - obstacle.location.y) / abs((linmidpoint.y - obstacle.location.y))) * linmidpoint.y * (obstacle.radiusMeters/ sqrt(linmidpoint.x + linmidpoint.y) )) / obstacle.radiusMeters,
                             from.rotation
 
                         )
@@ -149,11 +157,12 @@ class SwerveAutoDrive(
                             )
                     }
 
-
+                    //recursively check to make sure that the paths to and from the waypoint do not intersect an obstacle
                     return mutableListOf(pathfind(from, waypoint), pathfind(waypoint, to)).flatten().toMutableList()
                 }
             }
         }
+            //if they don't, just return the destination
             return mutableListOf(to)
     }
 }
