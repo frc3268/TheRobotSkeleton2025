8c7563ea18fe169282635d8b0c7c331397985e16
Igbalabanov <101612094+Chs000-00@users.noreply.github.com>
2025-01-06 16:40:47 -0800

    Update FieldPositions.kt with cords I found

--- src/main/kotlin/frc/lib/FieldPositions.kt
+++ src/main/kotlin/frc/lib/FieldPositions.kt
@@ -7,6 +7,36 @@ import java.util.function.Supplier
 data class FieldLocation(val red: Pose2d, val blue: Pose2d)
 data class Obstacle(val location: Pose2d, val radiusMeters: Double)
 
+// TODO: IMPLEMENT! Size is in inches.
+// +Z is up into the air from the carpet, +X is horizontal to the right (Based off the image below), +Y runs from the Field Border towards the REEFS.
+//For the Z-Rotation (I pressume?) 0° faces the red alliance station, 90° faces the non- scoring table side, and 180° faces the blue alliance station. 
+// For the X-Rotation, 0 is perpendicular to the Z plane, and 90 degrees is facing the carpet. Distances are measured to the center of the tag.
+// Distances are measured to the center of the tag.
+// AprilTag Cordinates found in page 4 at https://firstfrc.blob.core.windows.net/frc2025/FieldAssets/2025FieldDrawings-FieldLayoutAndMarking.pdf
+// ID X Y Z Z-Rotation X-Rotation
+// 1 657.37 25.80 58.50 126 0
+// 2 657.37 291.20 58.50 234 0
+// 3 455.15 317.15 51.25 270 0
+// 4 365.20 241.64 73.54 0 30
+// 5 365.20 75.39 73.54 0 30
+// 6 530.49 130.17 12.13 300 0
+// 7 546.87 158.50 12.13 0 0
+// 8 530.49 186.83 12.13 60 0
+// 9 497.77 186.83 12.13 120 0
+// 10 481.39 158.50 12.13 180 0
+// 11 497.77 130.17 12.13 240 0
+// 12 33.51 25.80 58.50 54 0
+// 13 33.51 291.20 58.50 306 0
+// 14 325.68 241.64 73.54 180 30
+// 15 325.68 75.39 73.54 180 30
+// 16 235.73 -0.15 51.25 90 0
+// 17 160.39 130.17 12.13 240 0
+// 18 144.00 158.50 12.13 180 0
+// 19 160.39 186.83 12.13 120 0
+// 20 193.10 186.83 12.13 60 0
+// 21 209.49 158.50 12.13 0 0
+// 22 193.10 130.17 12.13 300 0
+
 object FieldPositions {
     val speakerCenter = FieldLocation(Pose2d(15.256, 5.547868, 0.0.rotation2dFromDeg()),
             Pose2d(1.6096, 5.547868, 180.0.rotation2dFromDeg()))
