e3a905a3413aad5bba441f0294a64e66059da6d2
epshteinmatthew <robotmeatsuit@gmail.com>
2025-01-07 19:42:58 -0800

    close but this is breaking

--- src/main/kotlin/frc/robot/elevator/ElevatorIOKraken.kt
+++ src/main/kotlin/frc/robot/elevator/ElevatorIOKraken.kt
@@ -1,4 +1,57 @@
 package frc.robot.elevator
 
-class ElevatorIOKraken {
+import com.ctre.phoenix6.hardware.TalonFX
+import com.revrobotics.CANSparkLowLevel
+import com.revrobotics.CANSparkMax
+import edu.wpi.first.math.controller.PIDController
+import edu.wpi.first.units.AngleUnit
+import edu.wpi.first.units.Units
+import edu.wpi.first.units.measure.Angle
+import frc.lib.swerve.ElevatorIO
+
+class ElevatorIOKraken(override val pidController: PIDController) : ElevatorIO{
+    val leftMotor = TalonFX(0, "rio")
+    val rightMotor = TalonFX(0, "rio")
+
+
+    init{
+        //todo!
+        //leftEncoder.positionConversionFactor = 0.0
+        //rightEncoder.positionConversionFactor = 0.0
+
+    }
+    override fun updateInputs(inputs: ElevatorIO.ElevatorIOInputs) {
+        //this formula may need to me changed to reflect the reality
+        inputs.elevatorPositionMeters = (leftMotor.position.valueAsDouble + rightMotor.position.valueAsDouble) / 2
+        inputs.rightMotorPositionMeters = rightMotor.position.valueAsDouble
+        inputs.leftMotorPositionMeters = leftMotor.position.valueAsDouble
+        inputs.rightMotorCurrentAmps = doubleArrayOf(rightMotor.statorCurrent.valueAsDouble)
+        inputs.leftMotorCurrentAmps = doubleArrayOf(leftMotor.statorCurrent.valueAsDouble)
+        inputs.rightMotorAppliedVolts = rightMotor.motorVoltage.valueAsDouble
+        inputs.leftMotorAppliedVolts = leftMotor.motorVoltage.valueAsDouble
+        inputs.rightMotorVelocityMetersPerSec = rightMotor.velocity.valueAsDouble
+        inputs.leftMotorVelocityMetersPerSec = leftMotor.velocity.valueAsDouble
+    }
+
+    override fun setBothVolts(volts: Double) {
+        rightMotor.setVoltage(volts)
+        leftMotor.setVoltage(volts)
+    }
+
+    override fun reset() {
+        rightMotor.setPosition(Angle.ofRelativeUnits(0.0, Units.Degree))
+    }
+
+    override fun stop() {
+        rightMotor.stopMotor()
+        leftMotor.stopMotor()
+    }
+
+    override fun setLeftVolts(volts: Double) {
+        leftMotor.setVoltage(volts)
+    }
+
+    override fun setRightVolts(volts: Double) {
+        rightMotor.setVoltage(volts)
+    }
 }
