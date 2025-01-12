989eb9b5ee3d5b902dd5e4a4b644cb6cd55ccad7
epshteinmatthew <robotmeatsuit@gmail.com>
2025-01-11 20:01:00 -0800

    this really is how the story goes
    tldr it doesnt work yet

--- src/main/kotlin/frc/lib/pathfinder.kt
+++ src/main/kotlin/frc/lib/pathfinder.kt
@@ -1,5 +1,8 @@
 package frc.lib
 
+import edu.wpi.first.math.geometry.Pose2d
+import edu.wpi.first.units.Units
+import edu.wpi.first.units.Units.Inches
 import java.lang.Math.pow
 import java.util.PriorityQueue
 import kotlin.math.pow
@@ -12,20 +15,12 @@ fun get_g(loc: Pair<Int, Int>, from: Point?, g: Double): Double {
     var currentFrom = from
 
     while (currentFrom?.from != null) {
-        val distance = Math.sqrt(
-            (loc.first - currentFrom.loc.first).toDouble().pow(2) +
-                    (loc.second - currentFrom.loc.second).toDouble().pow(2)
-        )
-        currentG += distance
+        currentG++
         currentFrom = currentFrom.from
     }
 
     if (currentFrom != null) {
-        val distance = Math.sqrt(
-            (loc.first - currentFrom.loc.first).toDouble().pow(2) +
-                    (loc.second - currentFrom.loc.second).toDouble().pow(2)
-        )
-        currentG += distance
+        currentG++
     }
 
     return currentG
@@ -34,15 +29,15 @@ fun get_g(loc: Pair<Int, Int>, from: Point?, g: Double): Double {
 
 fun get_f(loc: Pair<Int, Int>, dest: Pair<Int, Int>) = pow(pow(loc.first.toDouble() - dest.first.toDouble(), 2.0) + pow(loc.second.toDouble() - dest.second.toDouble(), 2.0), 0.5)
 
-fun construct_path(closed:MutableList<Point>) : List<Pair<Int, Int>>{
+fun construct_path(closed:MutableList<Point>) : MutableList<Pose2d>{
     val first = closed.last()
-    val list = mutableListOf(first.loc)
+    val list = mutableListOf(Pose2d())
     var next = first.from
     while(next != null){
-        list.add(0,next.loc)
+        list.add(0,Pose2d(next.loc.first * 0.3 + 0.15,next.loc.first * 0.3 + 0.15, 0.0.rotation2dFromDeg()))
         next = next.from
     }
-    return list.toList()
+    return list
 }
 
 val directions = listOf(
@@ -57,7 +52,7 @@ val directions = listOf(
 )
 
 
-fun a_star(start:Pair<Int,Int>, end:Pair<Int,Int>, grid:List<List<Boolean>>): MutableList<Point>{
+fun a_star(start:Pair<Int,Int>, end:Pair<Int,Int>, grid:Array<Array<Boolean>>): MutableList<Point>{
     val allPoints: MutableMap<Pair<Int, Int>, Double> = mutableMapOf(
         start to (get_g(start, null, 0.0) + get_f(start, end))
     )
@@ -68,7 +63,7 @@ fun a_star(start:Pair<Int,Int>, end:Pair<Int,Int>, grid:List<List<Boolean>>): Mu
     while (rootNow.loc != end){
         for (direction in directions){
             if(rootNow.loc.second + direction.second in 0..grid.size-1){
-                if(rootNow.loc.first + direction.first in 0.. grid[rootNow.loc.second + direction.second].size-1 && grid[rootNow.loc.second + direction.second][rootNow.loc.first + direction.first] && !closed.map { it.loc }.contains(Pair(rootNow.loc.first + direction.first, rootNow.loc.second + direction.second))){
+                if(rootNow.loc.first + direction.first in 0.. grid[rootNow.loc.second + direction.second].size-1 && !grid[rootNow.loc.second + direction.second][rootNow.loc.first + direction.first] && !closed.map { it.loc }.contains(Pair(rootNow.loc.first + direction.first, rootNow.loc.second + direction.second))){
                     allPoints.set(Pair(rootNow.loc.first + direction.first, rootNow.loc.second + direction.second), (get_g(Pair(rootNow.loc.first + direction.first, rootNow.loc.second + direction.second), rootNow, 0.0) + get_f(start, end)))
                     openqueue.add(Point(Pair(rootNow.loc.first + direction.first, rootNow.loc.second + direction.second), rootNow))
                 }
@@ -81,11 +76,17 @@ fun a_star(start:Pair<Int,Int>, end:Pair<Int,Int>, grid:List<List<Boolean>>): Mu
     return closed
 }
 
-fun main() {
-    val whatevs = construct_path(a_star(Pair(0,0), Pair(3,3), listOf(
-        listOf(true, true, true, true),
-        listOf(false, false, true, true),
-        listOf(true, true, true, true),
-        listOf(true, true, true, true)
-    )))
+fun smoothPath(poses: MutableList<Pose2d>): MutableList<Pose2d>{
+    val movingA:MutableList<Pose2d> = mutableListOf()
+    //
+    for (i in 0..poses.size - 3){
+        movingA.add(
+            Pose2d(
+            (poses[i].x + poses[i + 1].x + poses[i + 2].x) / 3,
+                (poses[i].y + poses[i + 1].y + poses[i + 2].y) / 3,
+                0.0.rotation2dFromDeg()
+        )
+        )
+    }
+    return movingA
 }
--- src/main/kotlin/frc/robot/commands/SwerveAutoDrive.kt
+++ src/main/kotlin/frc/robot/commands/SwerveAutoDrive.kt
@@ -3,19 +3,27 @@ package frc.robot.commands
 import edu.wpi.first.math.MathUtil
 import edu.wpi.first.math.geometry.Pose2d
 import edu.wpi.first.math.trajectory.TrapezoidProfile
+import edu.wpi.first.wpilibj.Filesystem
 import edu.wpi.first.wpilibj.Timer
 import edu.wpi.first.wpilibj2.command.Command
+import frc.lib.*
 import frc.lib.FieldPositions.obstacles
-import frc.lib.Obstacle
-import frc.lib.rotation2dFromDeg
-import frc.lib.rotation2dFromRad
 import frc.lib.swerve.SwerveDriveBase
 import frc.lib.swerve.SwerveDriveConstants
 import frc.lib.swerve.SwerveDriveConstants.DrivetrainConsts.MAX_SPEED_METERS_PER_SECOND
 import frc.robot.Constants
+import kotlinx.serialization.ExperimentalSerializationApi
+import kotlinx.serialization.Serializable
+import kotlinx.serialization.json.Json
+import kotlinx.serialization.json.decodeFromStream
+import java.io.File
 import java.util.function.DoubleSupplier
 import java.util.function.Supplier
 import kotlin.math.*
+
+@Serializable
+data class gridFile(val grid: Array<Array<Boolean>>)
+
 
 /*
 SwerveAutoDrive, command for swerve driving which fuses setpoint following and remote control
@@ -31,7 +39,7 @@ class SwerveAutoDrive(
     private var start = 0.0
     var next = Pose2d()
     var startPose = drive.getPose()
-    private var tolerance: Pose2d = Pose2d(0.1,0.1, 10.0.rotation2dFromDeg())
+    private var tolerance: Pose2d = Pose2d(0.1, 0.1, 10.0.rotation2dFromDeg())
 
     init {
         addRequirements(drive)
@@ -43,29 +51,25 @@ class SwerveAutoDrive(
         points.add(0, drive.getPose())
         drive.field.getObject("points").setPoses(points)
         next = points[0]
-        start = Timer.getFPGATimestamp()
     }
 
     override fun execute() {
         /*collect speeds based on which controls are used*/
 
-        //may have to tune this
-        val xnew = startPose.x + (Timer.getFPGATimestamp() - start + 0.5)
-        val ynew = startPose.y + (Timer.getFPGATimestamp() - start + 0.5) * (next.y - startPose.y) / (next.x - startPose.x)
 
         val speeds = Pose2d(
-                SwerveDriveConstants.DrivetrainConsts.xPIDController.calculate(
-                    drive.getPose().x,
-                    TrapezoidProfile.State(xnew, 0.0)
-                ) * MAX_SPEED_METERS_PER_SECOND,
-                SwerveDriveConstants.DrivetrainConsts.yPIDController.calculate(
-                    drive.getPose().y,
-                    TrapezoidProfile.State(ynew, 0.0)
-                ) * MAX_SPEED_METERS_PER_SECOND,
-                (SwerveDriveConstants.DrivetrainConsts.thetaPIDController.calculate(
-                    drive.getPose().rotation.degrees,
-                    TrapezoidProfile.State(next.rotation.degrees, 0.0)
-                ) * SwerveDriveConstants.DrivetrainConsts.MAX_ANGULAR_VELOCITY_DEGREES_PER_SECOND).rotation2dFromDeg(),
+            SwerveDriveConstants.DrivetrainConsts.xPIDController.calculate(
+                drive.getPose().x,
+                TrapezoidProfile.State(next.x, 0.0)
+            ) * MAX_SPEED_METERS_PER_SECOND,
+            SwerveDriveConstants.DrivetrainConsts.yPIDController.calculate(
+                drive.getPose().y,
+                TrapezoidProfile.State(next.y, 0.0)
+            ) * MAX_SPEED_METERS_PER_SECOND,
+            (SwerveDriveConstants.DrivetrainConsts.thetaPIDController.calculate(
+                drive.getPose().rotation.degrees,
+                TrapezoidProfile.State(next.rotation.degrees, 0.0)
+            ) * SwerveDriveConstants.DrivetrainConsts.MAX_ANGULAR_VELOCITY_DEGREES_PER_SECOND).rotation2dFromDeg(),
 
             )
 
@@ -92,8 +96,6 @@ class SwerveAutoDrive(
             }
             index++
             next = points[index]
-            start = Timer.getFPGATimestamp()
-            startPose = drive.getPose()
         }
         return false
     }
@@ -101,80 +103,21 @@ class SwerveAutoDrive(
     override fun end(interrupted: Boolean) {
     }
 
+    @OptIn(ExperimentalSerializationApi::class)
     private fun pathfind(from: Pose2d, to: Pose2d): MutableList<Pose2d> {
-        //this whole thing could be faster...
-        val sortedObstacles = obstacles.sortedBy { it.location.translation.getDistance(from.translation) }
-        //get put the line from "from" to "to" into slope-intercept form
-        val m: Double = (to.y - from.y) / (to.x - from.x)
-        val b: Double = -m * to.x + to.y
-        for (obstacle in sortedObstacles) {
-            //get the relevant coefficients from the obstacle's quadratic equation, when substituting mx+b for y
-            val ntwo = b - obstacle.location.y
-            val obx = obstacle.location.x
-            //quadratic formula constants
-            val btwo = -obx * 2 + ntwo * 2 * m
-            val a = m.pow(2) + 1
-            val c = ntwo.pow(2) + obx.pow(2) - obstacle.radiusMeters.pow(2)
-            val det = btwo.pow(2) - 4 * a * c
-            if (det >= 0) {
-                //only go forward if there are intersections (that is, if the determinant is non-negative)
-                val intersection_a: Pose2d = if (from.x > obstacle.location.x) Pose2d(
-                    ((-btwo + sqrt(det)) / (2 * a)), (m * ((-btwo + sqrt(det)) / (2 * a)) + b), from.rotation
-                ) else Pose2d(
-                    ((-btwo - sqrt(det)) / (2 * a)), (m * ((-btwo - sqrt(det)) / (2 * a)) + b), from.rotation
+        val grid = Json.decodeFromStream<gridFile>(
+            File(Filesystem.getDeployDirectory().toString() + "/pathplanner/navgrid.json").inputStream()).grid
+        val path = smoothPath(
+            construct_path(
+                a_star(
+                    Pair((from.x / .3).toInt(), (from.y / .3).toInt()),
+                    Pair((to.x / .3).toInt(), (to.y / .3).toInt()),
+                    grid
                 )
-                if ((intersection_a.x in from.x..to.x || intersection_a.x in to.x..from.x) && (intersection_a.y in from.y..to.y || intersection_a.y in to.y..from.y)) {
-                    //only go forward if the intersection is actually between the points from and to
-                    val intersection_b = if (from.x < obstacle.location.x) Pose2d(
-                        ((-btwo + sqrt(det)) / (2 * a)), (m * ((-btwo + sqrt(det)) / (2 * a)) + b), from.rotation
-                    ) else Pose2d(
-                        ((-btwo - sqrt(det)) / (2 * a)), (m * ((-btwo - sqrt(det)) / (2 * a)) + b), from.rotation
-                    )
-
-
-                    //midpoint on the line between the two intersection points
-                    val linmidpoint = Pose2d(
-                        (intersection_b.x + intersection_a.x) / 2,
-                        (intersection_b.y + intersection_a.y) / 2,
-                        0.0.rotation2dFromDeg()
-                    )
-
-                    //how far we have to go to get to the edge of the circle from the linear midpoint
-                    val dist = obstacle.radiusMeters - abs(linmidpoint.translation.getDistance(obstacle.location.translation))
-
-                    //rates of change for x and y between the center of the obstacle circle and the linear midpoint, this is used to extend the linear midpoint to the edge of the circle
-                    var rates = Pose2d( (dist) * (((linmidpoint.x - obstacle.location.x) / abs((linmidpoint.x - obstacle.location.x))) * linmidpoint.x * (obstacle.radiusMeters/ sqrt(linmidpoint.x + linmidpoint.y)))/obstacle.radiusMeters ,
-                        (dist) * (((linmidpoint.y - obstacle.location.y) / abs((linmidpoint.y - obstacle.location.y))) * linmidpoint.y * (obstacle.radiusMeters/ sqrt(linmidpoint.x + linmidpoint.y) )) / obstacle.radiusMeters,
-                            from.rotation
-
-                    )
-
-                    var waypoint =
-                        Pose2d(
-                            linmidpoint.translation.plus(rates.translation),
-                            from.rotation
-                        )
-
-                    if(waypoint.x !in 0.0..16.4846 || waypoint.y !in 0.0..8.1026) {
-                        //if waypoint is outside of the map flip the way the rates go
-                        rates = Pose2d( (abs(obstacle.radiusMeters * 2 - dist)) * -(((linmidpoint.x - obstacle.location.x) / abs((linmidpoint.x - obstacle.location.x))) * linmidpoint.x * (obstacle.radiusMeters/ sqrt(linmidpoint.x + linmidpoint.y)))/obstacle.radiusMeters ,
-                            (abs(obstacle.radiusMeters * 2 - dist)) * -(((linmidpoint.y - obstacle.location.y) / abs((linmidpoint.y - obstacle.location.y))) * linmidpoint.y * (obstacle.radiusMeters/ sqrt(linmidpoint.x + linmidpoint.y) )) / obstacle.radiusMeters,
-                            from.rotation
-
-                        )
-                        waypoint =
-                            Pose2d(
-                                linmidpoint.translation.plus(rates.translation),
-                                from.rotation
-                            )
-                    }
-
-                    //recursively check to make sure that the paths to and from the waypoint do not intersect an obstacle
-                    return mutableListOf(pathfind(from, waypoint), pathfind(waypoint, to)).flatten().toMutableList()
-                }
-            }
-        }
-            //if they don't, just return the destination
-            return mutableListOf(to)
+            )
+        )
+        path.add(to)
+        path.add(0, from)
+        return path
     }
 }
