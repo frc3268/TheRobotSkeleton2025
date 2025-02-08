9461a86c0f6b55801aa81c0de62ffcca72cf8429
fernir3268 <116040382+fernir3268@users.noreply.github.com>
2025-02-07 16:39:26 -0800

    AlgaeIntakeIOKraken (No AdvantageKit tho)

--- src/main/kotlin/frc/robot/algaeintake/AlgaeIntakeIOKraken.kt
+++ src/main/kotlin/frc/robot/algaeintake/AlgaeIntakeIOKraken.kt
@@ -1,41 +1,61 @@
 package frc.robot.algaeintake
 
+import com.ctre.phoenix6.configs.TalonFXConfiguration
+import com.ctre.phoenix6.hardware.TalonFX
 import edu.wpi.first.math.controller.PIDController
 
 class AlgaeIntakeIOKraken(override val pidController: PIDController) : AlgaeIntakeIO {
+
+    val mainMotor = TalonFX(0, "rio")
+    val revMotor = TalonFX(0, "rio")
+    val jointMotor = TalonFX(0, "rio")
+
+    init{
+        val mainConfig = TalonFXConfiguration()
+        val revConfig = TalonFXConfiguration()
+        val jointConfig = TalonFXConfiguration()
+
+        mainMotor.configurator.apply(mainConfig)
+        revMotor.configurator.apply(revConfig)
+        jointMotor.configurator.apply(jointConfig)
+    }
+
     override fun updateInputs(inputs: AlgaeIntakeIO.Inputs) {
         TODO("Not yet implemented")
     }
 
     override fun setJointVoltage(voltage: Double) {
-        TODO("Not yet implemented")
+        jointMotor.setVoltage(voltage)
     }
 
     override fun setMainAndRevVoltage(voltage: Double) {
-        TODO("Not yet implemented")
+        setRevVolate(voltage)
+        setMainVoltage(-voltage)
     }
 
     override fun setRevVolate(voltage: Double) {
-        TODO("Not yet implemented")
+        revMotor.setVoltage(voltage)
     }
 
     override fun setMainVoltage(voltage: Double) {
-        TODO("Not yet implemented")
+        mainMotor.setVoltage(voltage)
     }
 
     override fun stop() {
-        TODO("Not yet implemented")
+        stopJoint()
+        stopRev()
+        stopMain()
     }
 
     override fun stopMain() {
-        TODO("Not yet implemented")
+        mainMotor.stopMotor()
     }
 
     override fun stopRev() {
-        TODO("Not yet implemented")
+        revMotor.stopMotor()
     }
 
     override fun stopJoint() {
-        TODO("Not yet implemented")
+        jointMotor.stopMotor()
     }
 }
