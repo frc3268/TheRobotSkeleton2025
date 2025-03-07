408a4842aae05b2a24d9a749d584b4a7af5e5924
epshteinmatthew <robotmeatsuit@gmail.com>
2025-03-06 19:35:34 -0800

    fix reset and stop

--- src/main/kotlin/frc/robot/coralintake/CoralIntakeSubsystem.kt
+++ src/main/kotlin/frc/robot/coralintake/CoralIntakeSubsystem.kt
@@ -16,13 +16,14 @@ class CoralIntakeSubsystem(val io: CoralIntakeIO) : SubsystemBase() {
     val jointVelocityRotPerMinEntry = troubleshootingTab.add("Joint Velocity RPM", 0.0).withPosition(3, 1).entry
     val intakeVelocityRotPerMinEntry = troubleshootingTab.add("Intake Velocity RPM", 0.0).withPosition(3, 2).entry
     var setpoint = 0.0
+    var stopped = false
 
     init{
         troubleshootingTab.add("outtake",outtake()).withWidget(BuiltInWidgets.kCommand)
         troubleshootingTab.add("go up",run{io.setJointVoltage(0.5)}).withWidget(BuiltInWidgets.kCommand)
         troubleshootingTab.add("reset", runOnce { io.reset() }).withWidget(BuiltInWidgets.kCommand)
 
-        troubleshootingTab.add("stop", runOnce { stopJoint() }).withWidget(BuiltInWidgets.kCommand)
+        troubleshootingTab.add("stop", runOnce { stop() }).withWidget(BuiltInWidgets.kCommand)
         troubleshootingTab.add("go to score",raiseToScore()).withWidget(BuiltInWidgets.kCommand)
     }
 
@@ -31,7 +32,10 @@ class CoralIntakeSubsystem(val io: CoralIntakeIO) : SubsystemBase() {
         jointAngleEntry.setDouble(inputs.jointAngle.degrees)
         jointVelocityRotPerMinEntry.setDouble(inputs.jointVelocityRPM)
         intakeVelocityRotPerMinEntry.setDouble(inputs.intakeVelocityRPM)
-        io.setJointVoltage(io.pidController.calculate(inputs.jointAngle.degrees, setpoint))
+
+        if(!stopped){
+            io.setJointVoltage(io.pidController.calculate(inputs.jointAngle.degrees, setpoint))
+        }
 
     }
 
@@ -40,6 +44,7 @@ class CoralIntakeSubsystem(val io: CoralIntakeIO) : SubsystemBase() {
     fun outtake(): Command = run{io.setIntakeVoltage(-0.3 * 12.0)}.withTimeout(1.5)
 
     fun raiseToScore(): Command = runOnce {
+        stopped = false
         setpoint = -70.0
     }
 
@@ -48,10 +53,14 @@ class CoralIntakeSubsystem(val io: CoralIntakeIO) : SubsystemBase() {
     }
 
     fun lower(): Command = runOnce {
+        stopped = false
         setpoint = 0.0
     }
 
-    fun stop(): Command = runOnce{ io.stop() }
+    fun stop(): Command = runOnce{
+        io.stop()
+        stopped = true
+    }
     fun stopJoint(): Command = runOnce{ io.stopJoint() }
     fun stopIntake(): Command = runOnce{ io.stopIntake() }
 
