72c0ae91c6553808e1f0083be7578b63e284131e
epshteinmatthew <robotmeatsuit@gmail.com>
2025-03-08 23:36:02 -0800

    teleop fix

--- src/main/kotlin/frc/lib/swerve/SwerveDriveBase.kt
+++ src/main/kotlin/frc/lib/swerve/SwerveDriveBase.kt
@@ -214,13 +214,22 @@ class SwerveDriveBase(startingPose: Pose2d) : SubsystemBase() {
     }
 
     fun constructModuleStatesFromChassisSpeeds(xSpeedMetersPerSecond: Double, ySpeedMetersPerSecond: Double, turningSpeedDegreesPerSecond: Double, fieldOriented: Boolean): Array<SwerveModuleState> {
-        val red = DriverStation.getAlliance().get() == DriverStation.Alliance.Red
         return kinematics.toSwerveModuleStates(
                     if (fieldOriented){
-                        ChassisSpeeds.fromFieldRelativeSpeeds(xSpeedMetersPerSecond, ySpeedMetersPerSecond, turningSpeedDegreesPerSecond.rotation2dFromDeg().radians, if (red) {(poseEstimator.estimatedPosition.rotation + 180.0.rotation2dFromDeg())} else {(poseEstimator.estimatedPosition.rotation)})
+                        ChassisSpeeds.fromFieldRelativeSpeeds(xSpeedMetersPerSecond, ySpeedMetersPerSecond, turningSpeedDegreesPerSecond.rotation2dFromDeg().radians, (poseEstimator.estimatedPosition.rotation))
                     }else{
                         ChassisSpeeds(xSpeedMetersPerSecond, ySpeedMetersPerSecond, turningSpeedDegreesPerSecond.rotation2dFromDeg().radians)}
             )
+    }
+
+    fun constructTeleopModuleStatesFromChassisSpeeds(xSpeedMetersPerSecond: Double, ySpeedMetersPerSecond: Double, turningSpeedDegreesPerSecond: Double, fieldOriented: Boolean): Array<SwerveModuleState> {
+        val red = DriverStation.getAlliance().get() == DriverStation.Alliance.Red
+        return kinematics.toSwerveModuleStates(
+            if (fieldOriented){
+                ChassisSpeeds.fromFieldRelativeSpeeds(xSpeedMetersPerSecond, ySpeedMetersPerSecond, turningSpeedDegreesPerSecond.rotation2dFromDeg().radians, if (red) {(poseEstimator.estimatedPosition.rotation + 180.0.rotation2dFromDeg())} else {(poseEstimator.estimatedPosition.rotation)})
+            }else{
+                ChassisSpeeds(xSpeedMetersPerSecond, ySpeedMetersPerSecond, turningSpeedDegreesPerSecond.rotation2dFromDeg().radians)}
+        )
     }
     fun stop() {
         for (mod in modules) {
