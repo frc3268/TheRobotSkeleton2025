7b19cdcae111b128073c27dfe1d3a1d3355fc87e
Igbalabanov <101612094+chs000-00@users.noreply.github.com>
2025-03-15 17:33:00 -0700

    Whoops

--- src/main/kotlin/frc/lib/led/LedStrip.kt
+++ src/main/kotlin/frc/lib/led/LedStrip.kt
@@ -3,16 +3,19 @@ package frc.lib.led
 import edu.wpi.first.wpilibj.AddressableLED
 import edu.wpi.first.wpilibj.AddressableLEDBuffer
 import edu.wpi.first.wpilibj.LEDPattern
+import edu.wpi.first.wpilibj.util.Color
+import edu.wpi.first.wpilibj2.command.Command
+import edu.wpi.first.wpilibj2.command.SubsystemBase
 
 /** Create an LedStrip controller
  *
  * @param[ledPort] The output port to use. Must be a PWM header, not MXP or DIO
  * @param[ledLength] The length of the LED buffer in pixels.
  */
-class LedStrip : SubsystemBase (
+class LedStrip(
     val ledPort: Int,
     val ledLength: Int
-) {
+) : SubsystemBase() {
 
     // PWM port 9
     // Must be a PWM header, not MXP or DIO
@@ -29,22 +32,25 @@ class LedStrip : SubsystemBase (
         ledStrip.setData(ledBuffer)
         ledStrip.start()
 
-        setDefaultCommand(applyPattern(LEDPattern.solid(Color.kBlack)).withName("Off"));
+        setDefaultCommand(applyPattern(LEDPattern.solid(Color.kBlack)).withName("Off"))
     }
 
-    /** Apply a [pattern] to the [LedStrip]. */
+    /** Apply a [pattern] to the [LedStrip].
+     * @param[pattern] the pattern to apply to the buffer
+     * @sample applyGradientPattern
+     */
     fun applyPattern(pattern: LEDPattern): Command = run {
         pattern.applyTo(ledBuffer)
     }
 
-    /** Apply a gradient [pattern] to the [LedStrip]. */
+    /** Apply a gradient pattern to the [LedStrip]. */
     fun applyGradientPattern(color1: Color, color2: Color): Command = runOnce {
-        val gradient = LEDPattern.gradient(LEDPattern.GradientType.kContinuous, color1, color2);
-        gradient.applyTo(ledBuffer)
+        val gradient = LEDPattern.gradient(LEDPattern.GradientType.kContinuous, color1, color2)
+        applyPattern(gradient)
     }
 
     override fun periodic() {
         // Periodically send the latest LED color data to the LED strip for it to display
-        ledStrip.setData(ledBuffer);
+        ledStrip.setData(ledBuffer)
     }
 }
