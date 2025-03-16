01f25bf82098897e7b58644d2a4a10cb545ea21d
Igbalabanov <101612094+chs000-00@users.noreply.github.com>
2025-03-15 20:00:01 -0700

    I plan to take over Robot.kt

--- src/main/kotlin/frc/lib/core/core.kt
+++ src/main/kotlin/frc/lib/core/core.kt
@@ -2,13 +2,27 @@ package frc.lib.core
 
 import edu.wpi.first.hal.FRCNetComm
 import edu.wpi.first.hal.HAL
+import edu.wpi.first.math.geometry.Pose2d
+import edu.wpi.first.wpilibj2.command.button.CommandXboxController
+import frc.lib.swerve.SwerveDriveBase
+import frc.robot.Constants
 
-fun coreInitialize() {
 
-    // initialize HAL
-    check(HAL.initialize(500, 0)) { "Failed to initialize. Terminating." }
+object robotCore {
+    /** The drive subsystem. Must be accessed after calling [initCore] */
+    lateinit var driveSubsystem: SwerveDriveBase
+    lateinit var driverController: CommandXboxController
 
-    // report robot language as Kotlin
-    // 6 Means kotlin in French
-    HAL.report(FRCNetComm.tResourceType.kResourceType_Language, 6)
+
+    fun initCore() {
+        // initialize HAL
+        check(HAL.initialize(500, 0)) { "Failed to initialize. Terminating." }
+
+        // report robot language as Kotlin
+        // 6 Means kotlin in French
+        HAL.report(FRCNetComm.tResourceType.kResourceType_Language, 6)
+
+        driveSubsystem = SwerveDriveBase(Pose2d());
+        driverController = CommandXboxController(Constants.OperatorConstants.DRIVER_CONTROLLER_PORT)
+    }
 }
